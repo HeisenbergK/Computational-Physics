cb8d0ba5f651d992c473e6cd7a346ca28a452df8
John Andrew Kypriotakis <johnykypriotakis@gmail.com>
2023-05-17 19:13:18 +0300

    Switched to OOP

---
+++ Ising/Antiferro_Ortho_Sim.py
@@ -0,0 +1,30 @@
+from numpy import linspace, divide
+import pandas as pd
+from multiprocessing.pool import Pool
+from Ising import Ising
+
+lattice = 16
+iterations = 1500
+
+
+def make_whole_simulation(T):
+    model = Ising(T, lattice, iterations, mesh="ortho", prop="antiferro", plots=False)
+    result = divide(model.simulate(), (iterations * lattice * lattice))
+    return {"Temperature": T, "Energy": result[0], "Magnetization": result[1]}
+
+
+if __name__ == "__main__":
+    temperature_points = 1000
+
+    df = pd.DataFrame(columns=["Temperature", "Energy", "Magnetization"])
+    T_sample = linspace(1.3, 3.5, temperature_points)
+
+    with Pool() as pool:
+        for result in pool.imap(make_whole_simulation, T_sample):
+            df = df.append(result, ignore_index=True)
+    df.sort_values(by=["Temperature"], inplace=True)
+
+    df.to_csv(
+        f"Ising/Results/Antiferro_Ortho_16_1500_Simulation_{temperature_points}.csv",
+        index=False,
+    )
--- Ising/Ising.py
+++ Ising/Ising.py
@@ -1,18 +1,34 @@
-from numpy import exp, meshgrid, sum, linspace, divide
+from numpy import exp, meshgrid, sum, linspace, diff
 from numpy.random import rand, randint
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import pandas as pd
-from multiprocessing.pool import Pool
+from matplotlib import colors
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
 
 
 class Ising:
     # initialization
-    def __init__(self, temperature, lattice, iterations, plots=True):
+    def __init__(
+        self, temperature, lattice, iterations, mesh="ortho", prop="ferro", plots=True
+    ):
         self.temperature = temperature
         self.lattice = lattice
         self.iterations = iterations
         self.plots = plots
+
+        if mesh == "ortho":
+            self.mesh = "ortho"
+        elif mesh == "hex":
+            self.mesh = "hex"
+        else:
+            raise Exception("Incorrect mesh")
+
+        if prop == "ferro":
+            self.prop = "ferro"
+        elif prop == "antiferro":
+            self.prop = "antiferro"
+        else:
+            raise Exception("Incorrect property")
 
     # energy of the crystal
     def E(self, crystal, lattice):
@@ -26,17 +42,21 @@ class Ising:
                     + crystal[(i - 1) % lattice, j]
                     + crystal[i, (j - 1) % lattice]
                 )
-                if (i + 1) % 2 == 0:
+                if self.mesh == "hex" and ((i + 1) % 2 == 0):
                     nb += (
                         crystal[(i - 1) % lattice, (j - 1) % lattice]
                         + crystal[(i + 1) % lattice, (j - 1) % lattice]
                     )
-                else:
+                elif self.mesh == "hex":
                     nb += (
                         crystal[(i + 1) % lattice, (j + 1) % lattice]
                         + crystal[(i - 1) % lattice, (j + 1) % lattice]
                     )
-                energy += nb * S
+
+                if self.prop == "ferro":
+                    energy += -nb * S
+                elif self.prop == "antiferro":
+                    energy += nb * S
         return energy / 6.0
 
     # magnetization of crystal
@@ -57,17 +77,22 @@ class Ising:
                     + crystal[(a - 1) % lattice, b]
                     + crystal[a, (b - 1) % lattice]
                 )
-                if (a + 1) % 2 == 0:
+                if self.mesh == "hex" and ((a + 1) % 2 == 0):
                     nb += (
                         crystal[(a - 1) % lattice, (b - 1) % lattice]
                         + crystal[(a + 1) % lattice, (b - 1) % lattice]
                     )
-                else:
+                elif self.mesh == "hex":
                     nb += (
                         crystal[(a + 1) % lattice, (b + 1) % lattice]
                         + crystal[(a - 1) % lattice, (b + 1) % lattice]
                     )
-                metric = -2 * s * nb
+
+                if self.prop == "ferro":
+                    metric = 2 * s * nb
+                elif self.prop == "antiferro":
+                    metric = -2 * s * nb
+
                 if metric < 0:
                     s *= -1
                 elif rand() < exp(-metric * beta):
@@ -81,13 +106,27 @@ class Ising:
         M = 0
         crystal = 2 * randint(2, size=(self.lattice, self.lattice)) - 1
 
-        if self.plots:
+        if self.plots and self.mesh == "hex":
+            f = plt.figure(figsize=(15, 15))
+            self.configPlotHex(f, crystal, 0, self.lattice, 1)
+        elif self.plots and self.mesh == "ortho":
             f = plt.figure(figsize=(15, 15))
             self.configPlot(f, crystal, 0, self.lattice, 1)
 
         for i in range(self.iterations):
             self.mcStep(crystal, self.lattice, 1.0 / self.temperature)
-            if self.plots:
+            if self.plots and self.mesh == "hex":
+                if i == 1:
+                    self.configPlotHex(f, crystal, i, self.lattice, 2)
+                if i == 4:
+                    self.configPlotHex(f, crystal, i, self.lattice, 3)
+                if i == 32:
+                    self.configPlotHex(f, crystal, i, self.lattice, 4)
+                if i == 100:
+                    self.configPlotHex(f, crystal, i, self.lattice, 5)
+                if i == self.iterations - 1:
+                    self.configPlotHex(f, crystal, i, self.lattice, 6)
+            elif self.plots and self.mesh == "ortho":
                 if i == 1:
                     self.configPlot(f, crystal, i, self.lattice, 2)
                 if i == 4:
@@ -106,55 +145,43 @@ class Ising:
 
         return [E, M]
 
-    # make a single plot
-    def configPlot(self, f, crystal, i, lattice, n_):
-        X, Y = meshgrid(range(lattice), range(lattice))
-        X, Y = X * 2, Y * 2
+    # configure hex plot
+    def configPlotHex(self, f, crystal, i, lattice, n_):
+        nx = lattice
+        ny = lattice
+        cmap = colors.ListedColormap(["black", "white"])
+        bounds = [0, 1, 2]
+        norm = colors.BoundaryNorm(bounds, cmap.N)
+        x = linspace(0, 1, nx)
+        y = linspace(0, 1, ny)
 
-        # Turn this into a hexagonal grid
-        for ii, k in enumerate(X):
-            if ii % 2 == 1:
-                X[ii] += 1
-                Y[:, ii] += 1
+        dx = diff(x)[0]
+
+        patches = []
+        for k in x:
+            for n, j in enumerate(y):
+                if n % 2:
+                    polygon = mpatches.RegularPolygon([k - dx / 2.0, j], 6, 0.6 * dx)
+                else:
+                    polygon = mpatches.RegularPolygon([k, j], 6, 0.6 * dx)
+                patches.append(polygon)
+
+        collection = PatchCollection(patches, cmap=cmap, norm=norm, alpha=1.0)
 
         sp = f.add_subplot(3, 3, n_)
         sp.set_xticks([])
         sp.set_yticks([])
-        im = sp.hexbin(
-            X.reshape(-1),
-            Y.reshape(-1),
-            C=crystal.reshape(-1),
-            gridsize=int(crystal.shape[0] / 2),
-            cmap=plt.cm.binary,
-            linewidths=0.0,
-        )
+        sp.add_collection(collection)
+        collection.set_array(crystal.ravel())
         sp.set_title("Time=%d" % i)
         sp.axis("tight")
-        divider = make_axes_locatable(sp)
-        cax = divider.append_axes("right", size="5%", pad=0.05)
-        f.colorbar(im, cax=cax, orientation="vertical")
 
-
-def make_whole_simulation(T, lattice=16, iterations=1500):
-    model = Ising(T, lattice, iterations, False)
-    result = divide(model.simulate(), (iterations * lattice * lattice))
-    return {"Temperature": T, "Energy": result[0], "Magnetization": result[1]}
-
-
-if __name__ == "__main__":
-    temperature_points = 1000
-
-    df = pd.DataFrame(columns=["Temperature", "Energy", "Magnetization"])
-    T_sample = linspace(0.01, 5.5, temperature_points)
-    lattice = 16
-    iterations = 1500
-
-    with Pool() as pool:
-        for result in pool.imap(make_whole_simulation, T_sample):
-            df = df.append(result, ignore_index=True)
-    df.sort_values(by=["Temperature"], inplace=True)
-
-    df.to_csv(
-        f"Ising/Results/Antiferro_Hex_{lattice}_{iterations}_Simulation_{temperature_points}.csv",
-        index=False,
-    )
+    # make a single plot
+    def configPlot(self, f, crystal, i, lattice, n_):
+        X, Y = meshgrid(range(lattice), range(lattice))
+        sp = f.add_subplot(3, 3, n_)
+        plt.setp(sp.get_yticklabels(), visible=False)
+        plt.setp(sp.get_xticklabels(), visible=False)
+        plt.pcolormesh(X, Y, crystal, cmap=plt.cm.binary)
+        plt.title("Time=%d" % i)
+        plt.axis("tight")
